b935d18e3d092bebd3549a47272ffa006865281f
CaioGama00 <caioesao@gmail.com>
2025-11-06 09:46:31 -0300

    feat: implementa logica de encontrar a maior palavra

--- soluções.py
+++ soluções.py
@@ -6,6 +6,11 @@ def cifra_de_cesar(texto, deslocamento):
     # TODO:
     pass
 
-def valida_cpf(cpf_string):
-    # TODO:
-    pass
+def encontrar_maior_palavra(frase):
+    x = frase.split(" ")
+    y = ""
+    for i in x:
+        if len(i) > len(y):
+            y = i
+    return y
+    
