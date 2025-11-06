a49f6d1ceb883772757314fe00b06e5949e971ec
ReKaji <renankajihara@gmail.com>
2025-11-06 12:59:47 -0300

    Implementação da função sao_anagramas

--- soluções.py
+++ soluções.py
@@ -1,11 +1,14 @@
 def sao_anagramas(string1, string2):
-    # TODO:
-    pass
+    string1 = string1.replace(" ", "").lower()
+    string2 = string2.replace(" ", "").lower()
+    string1 = sorted(string1)
+    string2 = sorted(string2)
+    return string1 == string2
 
 def cifra_de_cesar(texto, deslocamento):
     # TODO:
     pass
-
+    
 def valida_cpf(cpf_string):
     # TODO:
     pass
